a7f8a9b87809c7a9d14d7be2e329f1637a2ce33b
Mark Marushak <mark@example.com>
2026-01-22 10:45:24 +0500

    Fix base path to /Nodus

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://DeadlyDeadlars.github.io',
-  base: '/nodus',
+  base: '/Nodus',
   outDir: './deploy',
   i18n: {
     defaultLocale: 'ru',
